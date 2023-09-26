03afce2be7da62c3d7919b1ec1905e1be2c00d58
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-09-26 17:37:21 +0900

    [fix] 氷のプレハブ修正、オーディオマネージャーの雛型修正

--- Assets/Yoshida/AudioManager.cs
+++ Assets/Yoshida/AudioManager.cs
@@ -4,6 +4,9 @@ public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioClip _hitBlockIceSE;
     [SerializeField] AudioClip _hitIceCreamSE;
+    [SerializeField] AudioClip _dropIceCreamSE;
+    [SerializeField] AudioClip _jumpSE;
+    [SerializeField] AudioClip _buttonClickSE;
     AudioSource _audioSource;
     void Start()
     {
@@ -11,4 +14,7 @@ public class AudioManager : MonoBehaviour
     }
     public void HitBlockIceSE() => _audioSource.PlayOneShot(_hitBlockIceSE);
     public void HitIceCreamSE() => _audioSource.PlayOneShot(_hitIceCreamSE);
+    public void DropIceCreanSE() => _audioSource.PlayOneShot(_dropIceCreamSE);
+    public void JumpSE() => _audioSource.PlayOneShot(_jumpSE);
+    public void ButtonClickSE() => _audioSource.PlayOneShot(_buttonClickSE);
 }
