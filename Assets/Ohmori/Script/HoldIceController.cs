b01091a70ef5f9f4d336d01181d136136d3aab72
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2023-09-26 15:41:09 +0900

    [add] Iceをつむ機能の追加

---
+++ Assets/Ohmori/Script/HoldIceController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+using UnityEngine;
+
+public class HoldIceController : MonoBehaviour
+{
+    [SerializeField, Tooltip("詰める際の速度")] private float _duration = 0.5F;
+    /// <summary>このオブジェクトのインデックス</summary>
+    private int _index = 0;
+
+    private bool _isMoveing = false;
+
+    private Vector3 _lastTarget = Vector3.zero;
+    private TweenerCore<Vector3, Vector3, VectorOptions> _callback = default;
+
+    public int Index
+    {
+        get => _index;
+        set => _index = value;
+    }
+
+    public void IndexDecriment(Vector3 moveTarget)
+    {
+        if (_isMoveing)
+        {
+            _callback.Kill();
+            transform.position = _lastTarget;
+            _isMoveing = false;
+        }
+        _index--;
+        _isMoveing = true;
+        _lastTarget = moveTarget;
+        _callback = transform.DOMove(moveTarget, _duration).OnComplete(() => _isMoveing = false);
+    }
+}
--- Assets/Ohmori/Script/PlayerController.cs
+++ Assets/Ohmori/Script/PlayerController.cs
@@ -10,6 +10,12 @@ public class PlayerController : MonoBehaviour
     
     [SerializeField, Tooltip("何秒でHPが0になるか(何もしなかった際)")]
     private float _slipDamage = 30.0F;
+
+    [SerializeField, Tooltip("アイスを積み重ねるPosition")]
+    private Transform[] _iceHoldPositions = default;
+
+    [SerializeField, Tooltip("積み重ねるIceのPrefab")]
+    private HoldIceController _icePrefab = default;
     
     /// <summary>現在のHP</summary>
     private ReactiveProperty<float> _currentHpRP = new ReactiveProperty<float>();
@@ -18,14 +24,39 @@ public class PlayerController : MonoBehaviour
     public IReadOnlyReactiveProperty<float> CurrentHpRP => _currentHpRP;
 
     private PlayerMoveController _moveController = default;
+    private List<HoldIceController> _iceControllers = new List<HoldIceController>();
+    private int _currentIceIndex = 0;
     
-    public void HitBlockIce(float _healValue, float _addSpeed)
+    public void HitBlockIce(float healValue, float addSpeed)
     {
+        // HPの更新
+        _currentHpRP.Value = Mathf.Clamp(_currentHpRP.Value + healValue, 0, _maxHP);
         
+        // スピードの更新
+        _moveController.AddSpeed(addSpeed);
     }
-    
+
     public void HitIceCream()
-    {}
+    {
+        var temp = Instantiate(_icePrefab);
+        temp.transform.position = _iceHoldPositions[_currentIceIndex].position;
+        _iceControllers.Add(temp);
+        temp.Index = _currentIceIndex;
+        _currentIceIndex++;
+    }
+
+    public void HitEnemy(int removeIndex)
+    {
+        var temp = _iceControllers[removeIndex];
+        _iceControllers.RemoveAt(removeIndex);
+
+        for (int i = removeIndex; i < _iceControllers.Count; i++)
+        {
+            _iceControllers[i].IndexDecriment(_iceHoldPositions[i].position);
+        }
+
+        _currentIceIndex--;
+    }
 
     private void Awake()
     {
