305f9e1a6abccf5b0710fbf19498b570e1340086
Octorok192 <awmcco893@i.softbank.jp>
2023-09-27 13:59:15 +0900

    ステージの仮組が完成した。

--- Assets/Shigetomi/Script/SceneLoad.cs
+++ Assets/Shigetomi/Script/SceneLoad.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 public class SceneLoad : MonoBehaviour
 {
     [SerializeField] string _sceneName;
+    [SerializeField] ScoreManager _scoreManager;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            _scoreManager.CalcScore(true);
             SceneManager.LoadScene(_sceneName);
         }
     }
